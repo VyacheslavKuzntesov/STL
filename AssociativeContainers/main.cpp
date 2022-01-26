08b2fb9ace0eac0505c49d23075c9627a65f6ff0
VyacheslavKuzntesov <kuznetsov1997Vyacheslav@yandex.ru>
2022-01-26 17:19:21 +0300

    Police day 1

---
+++ AssociativeContainers/main.cpp
@@ -0,0 +1,190 @@
+﻿#include<iostream>
+#include<fstream>
+#include<set>
+#include<map>
+#include<string>
+#include<list>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+
+//#define STL_SET
+//#define STL_MAP
+//#define STL_MAP_PRINT
+
+void print(const std::map<std::string, std::list<std::string>>& dictionary);
+void save(const std::map<std::string, std::list<std::string>>& dictionary);
+void load(std::map<std::string, std::list<std::string>>& dictionary);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef STL_SET
+	std::set<int> set = { 50,25,75,16,32,48,64,85,80,50,25,75 };
+	for (std::set<int>::iterator it = set.begin(); it != set.end(); it++)cout << *it << tab;
+	cout << endl;
+	int value;
+	cout << "Введите добовляемое значение: "; cin >> value;
+	set.insert(value);
+	for (std::set<int>::iterator it = set.begin(); it != set.end(); it++)cout << *it << tab;
+	cout << endl;
+#endif // STL_SET
+
+#ifdef STL_MAP
+	std::map<int, std::string> week =
+	{
+		std::pair<int,std::string>(0,"Sunday"),
+		std::pair<int,std::string>(1,"Monday"),
+		std::pair<int,std::string>(2,"Tuesday"),
+		std::pair<int,std::string>(3,"Wednesday"),
+		std::pair<int,std::string>(4,"Thursday"),
+		std::pair<int,std::string>(5,"Friday"),
+		std::pair<int,std::string>(6,"Saturday"),
+		std::pair<int,std::string>(0,"Sunday"),
+		std::pair<int,std::string>(1,"Sunday"),
+		std::pair<int,std::string>(1,"Sunday"),
+		std::pair<int,std::string>(7,"Sunday"),
+	};
+
+	for (std::map<int, std::string>::iterator it = week.begin(); it != week.end(); it++)
+	{
+		//			ключ			   значение
+		cout << it->first << tab << it->second << endl;
+	}
+	cout << week[2] << endl;
+	cout << week[8] << endl;
+	for (auto& kv : week)
+	{
+		cout << kv.first << tab << kv.second << endl;
+	}
+	cout << "\n-------------------------------------------------------\n";
+	try
+	{
+		cout << week.at(9) << endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << endl;
+	}
+	for (std::pair<int, std::string>i : week)
+	{
+		cout << i.first << tab << i.second << endl;
+	}
+#endif // STL_MAP
+
+	std::map<std::string, std::list<std::string>> dictionary /*=
+	{
+		std::pair<std::string,std::list<std::string>>("asset",{"ценное качество","полезное свойство"}),
+		std::pair<std::string,std::list<std::string>>("space",{"космос","пространство"}),
+		std::pair<std::string,std::list<std::string>>("distinct",{"отдельный","особый","отличный"}),
+		std::pair<std::string,std::list<std::string>>("break",{"тормоз","разбивать","ломать"}),
+		std::pair<std::string,std::list<std::string>>("deer",{"олень","лань"})
+	}*/;
+
+	load(dictionary);
+
+	print(dictionary);
+	//save(dictionary);
+
+#ifdef STL_MAP_PRINT
+	/*for (std::pair<std::string, std::list<std::string>>i : dictionary)
+{
+	cout << i.first << tab;
+	for (std::string i : i.second)cout << i << tab; cout << endl;
+}*/
+/*for (std::map<std::string, std::list<std::string>>::iterator it = dictionary.begin(); it != dictionary.end(); it++)
+{
+	cout.width(10);
+	cout << std::left;
+	cout << it->first + ":";
+	for (std::list<std::string>::iterator l_it = it->second.begin(); l_it != it->second.end(); l_it++)
+	{
+		cout << *l_it << ", ";
+	}
+	cout << "\b\b;\n";
+}*/
+/*for (std::pair<std::string, std::list<std::string>>i : dictionary)
+{
+	cout.width(10);
+	cout << std::left;
+	cout << i.first + ":";
+	for (std::string j : i.second)cout << j << ", ";
+	cout << "\b\b;\n";
+}*/
+#endif // STL_MAP_PRINT
+
+}
+
+void print(const std::map<std::string, std::list<std::string>>& dictionary)
+{
+	for (std::pair<std::string, std::list<std::string>>i : dictionary)
+	{
+		cout.width(10);
+		cout << std::left;
+		cout << i.first + ":";
+		for (std::string j : i.second)cout << j << ", ";
+		cout << "\b\b;\n";
+	}
+}
+
+void save(const std::map<std::string, std::list<std::string>>& dictionary)
+{
+	std::ofstream fout("file.txt");
+	for (std::pair<std::string, std::list<std::string>>i : dictionary)
+	{
+		fout.width(10);
+		fout << std::left;
+		fout << i.first + ":";
+		for (std::string j : i.second)fout << j << ", ";
+		fout.seekp(-2, std::ios::cur);
+		//cur (current) - текущая позиция
+		fout << ";\n";
+	}
+	fout.close();
+	system("start notepad file.txt");
+}
+
+void load(std::map<std::string, std::list<std::string>>& dictionary)
+{
+	std::ifstream fin("file.txt");
+	if (fin.is_open())
+	{
+		while (!fin.eof())
+		{
+			std::string key;
+			std::string value;
+			std::getline(fin, key, ':');
+			dictionary[key];
+			std::getline(fin, value);
+			for (size_t start = 0, end = 0/*value.find(',')*/; value.find(',', start) != std::string::npos; start = end)
+			{
+				end = value.find(',', start+1);
+				std::string buffer = value.substr(start, end - start);
+				buffer.erase(0, buffer.find_first_not_of(','));
+				buffer.erase(0, buffer.find_first_not_of(' '));
+				buffer.erase(buffer.find_last_not_of(';')+1,buffer.size());
+				buffer.erase(buffer.find_last_not_of(' ')+1,buffer.size());
+				dictionary[key].push_back(buffer);
+			}
+		}
+		fin.close();
+		dictionary.erase("");
+	}
+	else
+	{
+		std::cerr << "Error: file not found" << endl;
+	}
+}
+
+
+/*
+АССОЦИАТИВНЫЕ КОНТЕЙНЕРЫ
+Ассоциативные контейнеры хранят данные в виде бинарного дерева к ассоцитивным контейнерам относатся map, set, multimap, multiset
+Основным отличием между map и set является то что set хранит просто набор однотипных значений а map хранит пары типа ключ значения pair: key - value
+А значит основным отличием между map multimap set multiset заключается в том что map и set это уникальгые контейнеры которые не допускают повторяющехся значений а в multimap multiset ключи значения могут встречаться больше одного раза
+
+
+*/
--- STL/main.cpp
+++ STL/main.cpp
@@ -1,6 +1,9 @@
 ﻿#include<iostream>
 #include<array>
 #include<vector>
+#include<deque>
+#include<forward_list> //односвязный список
+#include<list> // двусвязный список
 
 //using namespace std;
 using std::cin;
@@ -11,6 +14,10 @@ using std::endl;
 
 //#define STL_ARRAY
 //#define STL_VECTOR
+//#define STL_HOMEWORK
+//#define STL_DEQUE
+//#define STL_FORWARD_LIST
+//#define STL_LIST
 
 void main()
 {
@@ -60,8 +67,34 @@ void main()
 	cout << "Size:	  " << d_vec.size() << endl;
 	cout << "Capacity:" << d_vec.capacity() << endl;
 	cout << "MaxSize: " << d_vec.max_size() << endl;*/
+
+	int value;
+	int count;
+	int index;
+	/*cout << "Введите индекс добовляемого элемента: "; cin >> index;
+	cout << "Введите количество добавлений: "; cin >> count;
+	cout << "Введите значение добовляемого элемента: "; cin >> value;
+
+	if (index <= vec.size())vec.insert(vec.begin() + index, count ,value);
+	else std::cerr << "Error: out of range." << endl;*/
+
+	vec.insert(vec.begin() + 11, { 256,512,768 });
+
+	for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+
+	for (std::vector<int>::reverse_iterator it = vec.rbegin(); it != vec.rend(); ++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+
 #endif // STL_VECTOR
 
+#ifdef STL_HOMEWORK
 	std::vector<int> vec = { 0,1,1,2,3,5,8,13,21,34 };
 	int value;
 	int index;
@@ -74,13 +107,97 @@ void main()
 		cout << vec.data()[i] << tab;
 	}
 
-	cout << endl <<"Введите индекс удаляемого значения: "; cin >> index;
+	cout << endl << "Введите индекс удаляемого значения: "; cin >> index;
 	vec.erase(vec.cbegin() + index);
 
 	for (int i = 0; i < vec.capacity(); i++)
 	{
 		cout << vec.data()[i] << tab;
 	}
+#endif // STL_HOMEWORK
+
+#ifdef STL_DEQUE
+	std::deque<int> deque = { 3,5,8,13,21 };
+	deque.push_back(34);
+	deque.push_back(55);
+
+	deque.push_front(2);
+	deque.push_front(1);
+	deque.push_front(1);
+	deque.push_front(0);
+
+	for (std::deque<int>::iterator it = deque.begin(); it != deque.end(); ++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+
+	for (std::deque<int>::reverse_iterator it = deque.rbegin(); it != deque.rend(); ++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+
+
+#endif // STL_DEQUE
+
+#ifdef STL_FORWARD_LIST
+	std::forward_list<int> list1 = { 3,5,8,13,21 };
+	list1.push_front(2);
+	list1.push_front(1);
+	list1.insert_after(list1.begin(),1);
+	list1.insert_after(list1.before_begin(),0);
+
+	list1.reverse();
+	list1.push_front(34);
+	list1.reverse();
+
+	for (std::forward_list<int>::iterator it = list1.begin(); it != list1.end(); ++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+
+	std::forward_list<int> list2 = { 256,512,768 };
+	for (int i : list2)cout << i << tab; cout << endl;
+
+	list1.swap(list2);
+	for (int i : list1)cout << i << tab; cout << endl;
+	for (int i : list2)cout << i << tab; cout << endl;
+#endif // STL_FORWARD_LIST
+
+#ifdef STL_LIST
+	std::list<int> list = { 3,5,8,13,21 };
+	for (std::list<int>::iterator it = list.begin(); it != list.end(); ++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+	for (std::list<int>::reverse_iterator it = list.rbegin(); it != list.rend(); ++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+
+	int index;
+	int value;
+	cout << "Введите индекс добовляемого элемента: "; cin >> index;
+	cout << "Введите значение добовляемого элемента: "; cin >> value;
+
+	std::list<int>::iterator position = list.begin();
+	//for (int i = 0; i < index; i++)position++;
+	if (index <= list.size())
+	{
+		std::advance(position, index);
+		list.insert(position, value);
+	}
+	else cout << "Error: out of range\n";
+
+	for (int i : list)cout << i << tab; cout << endl;
+
+#endif // STL_LIST
+
+
 }
 
 
@@ -124,4 +241,11 @@ N - количество элементов
 vector - это контейнер который хранит данные в виде динамического массива
 	std::vector<T>
 T - тип элементов
+
+--------------------------------------------------------------------------------------
+deque(double-ended queue или двунаправленная очередь) - это линейный контейнер который хранит данные в виде списка динамических массивов(списка векторов).
+Это не много ускоряет операции добавление и удаление элементов но немного замедляет доступ к элементам
+В отличии от вектора deque позволяет добавлять элементы не только в конец но и в начало контейнера
+
+--------------------------------------------------------------------------------------
 */
